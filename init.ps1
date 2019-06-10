b7dd80b2d8383e521bb36b5400ede9578582c7f2
Zach <zwc101@gmail.com>
2019-06-09 20:43:12 -0700

    adding additional requirements

--- init.ps1
+++ init.ps1
@@ -2,7 +2,6 @@
 # After dot sourcing it ". init.ps1", all processes launched in that session will have access to your secrets as environment variables.
 # This is also a good place to do initial dev setup, provided you do it idempotently.
 
-
 $KeyVaultName = "KEYVAULTNAME"
 
 Get-AzureKeyVaultSecret -VaultName $KeyVaultName `
@@ -14,3 +13,11 @@ if (!(Get-Module "Requirements" -ListAvailable)) {
     Install-Module "Requirements" -Scope "CurrentUser" -Force
 }
 Import-Module "Requirements"
+
+Invoke-ChecklistRequirement -Describe "Install Pester" `
+    -Test { (Get-Module "Pester" -ListAvailable) } `
+    -Set { Install-Module "Pester" -Force }
+
+Invoke-ChecklistRequirement -Describe "Set Module Path" `
+    -Test { ("./src/modules" -notin $env:PSModulePath) }
+    -Set { $env:PSModulePath += ":./src/modules" }
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Baseline
+
+This is a template repo.  It is intended to get you up and running with a high quality Powershell
+
+```powershell
+code .
+. init.ps1
+```
